97a9fa232664a957dfa79287ae28c2472fda1d16
guptasurbhi99 <gupta.surbhi99@gmail.com>
2017-10-30 22:44:15 +0530

    Added division method

--- calc.c
+++ calc.c
@@ -7,6 +7,9 @@ scanf("%d",&a);
 scanf("%d",&b);
 int sum = a + b;
   int mul = a*b;
+  int div=a/b;
 printf("\n Sum = %d",&sum);
   printf("\n Product = "%d", & mul) ;
+         printf("\n division = "%d", & div) ;
+         
 }
