b14f7b70bce815a91cc1a6830c7710d87107a404
guptasurbhi99 <gupta.surbhi99@gmail.com>
2017-10-30 22:43:06 +0530

    Added Mod method

--- calc.c
+++ calc.c
@@ -7,6 +7,9 @@ scanf("%d",&a);
 scanf("%d",&b);
 int sum = a + b;
   int mul = a*b;
+  int mod=a%b;
 printf("\n Sum = %d",&sum);
   printf("\n Product = "%d", & mul) ;
+         printf("\n Mod = "%d", & mod) ;
+         
 }
